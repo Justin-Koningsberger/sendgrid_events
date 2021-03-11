b793a3f614961677951c8b55cceee16bfc630e93
Justin <justin.koningsberger@gmail.com>
2021-03-11 14:08:04 +0100

    Show events

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get '/sg_events', to: 'sg_events#index'
-  get '/sg_events/:id', to: 'sg_events#show'
+  resources :sg_events
   get '/search', to: 'sg_events#index'
   post '/endpoint', to: 'sg_events#create'
 end
